5e10c4c38aa7c0d94d7435536f60bd61c075f6db
lisheng.lisheng <lisheng.lisheng@alibaba-inc.com>
2015-12-01 01:06:19 +0800

    comment

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -1,100 +1,111 @@
 var Movie = require('../models/movie');
+var Comment = require('../models/comment');
+
 var _ = require('underscore');
 
 
 exports.detail = function (req, res) {
-    var id = req.params.id;
-    Movie.findById(id, function (err, movie) {
+  var id = req.params.id;
+  Movie.findById(id, function (err, movie) {
+    Comment
+      .find({movie: id})
+      .populate('from', 'name')
+      .populate('reply.from reply.to')
+      .exec(function (err, comments) {
+        console.log(comments);
         res.render('detail', {
-            title: '详情页',
-            movie: movie
+          title: '详情页',
+          movie: movie,
+          comments: comments
         });
-    });
+      });
+
+  });
 };
 
 exports.admin = function (req, res) {
-    res.render('admin', {
-        title: '后台录入页',
-        movie: {
-            title: '',
-            doctor: '',
-            country: '',
-            year: '',
-            poster: '',
-            flash: '',
-            summary: '',
-            language: ''
-        }
-    });
+  res.render('admin', {
+    title: '后台录入页',
+    movie: {
+      title: '',
+      doctor: '',
+      country: '',
+      year: '',
+      poster: '',
+      flash: '',
+      summary: '',
+      language: ''
+    }
+  });
 };
 
 exports.update = function (req, res) {
-    var id = req.params.id;
-    if (id) {
-        Movie.findById(id, function (err, movie) {
-            res.render('admin', {
-                title: '后台更新页',
-                movie: movie
-            });
-        })
-    }
+  var id = req.params.id;
+  if (id) {
+    Movie.findById(id, function (err, movie) {
+      res.render('admin', {
+        title: '后台更新页',
+        movie: movie
+      });
+    })
+  }
 };
 
 exports.save = function (req, res) {
-    var id = req.body.movie._id;
-    var _movie = req.body.movie;
-    var __movie;
+  var id = req.body.movie._id;
+  var _movie = req.body.movie;
+  var __movie;
 
-    if (id) {
-        Movie.findById(id, function (err, movie) {
-            if (err) console.log(err);
-            __movie = _.extend(movie, _movie);
-            __movie.save(function (err, movie) {
-                if (err) console.log(err);
-                res.redirect('/movie/' + movie._id);
-            });
+  if (id) {
+    Movie.findById(id, function (err, movie) {
+      if (err) console.log(err);
+      __movie = _.extend(movie, _movie);
+      __movie.save(function (err, movie) {
+        if (err) console.log(err);
+        res.redirect('/movie/' + movie._id);
+      });
 
-        });
-    } else {
-        __movie = new Movie({
-            doctor: _movie.doctor,
-            title: _movie.title,
-            country: _movie.country,
-            language: _movie.language,
-            year: _movie.year,
-            poster: _movie.poster,
-            summary: _movie.summary,
-            flash: _movie.flash
-        });
+    });
+  } else {
+    __movie = new Movie({
+      doctor: _movie.doctor,
+      title: _movie.title,
+      country: _movie.country,
+      language: _movie.language,
+      year: _movie.year,
+      poster: _movie.poster,
+      summary: _movie.summary,
+      flash: _movie.flash
+    });
 
-        __movie.save(function (err, movie) {
-            if (err) console.log(err);
-            res.redirect('/movie/' + movie._id);
-        });
-    }
+    __movie.save(function (err, movie) {
+      if (err) console.log(err);
+      res.redirect('/movie/' + movie._id);
+    });
+  }
 };
 
 exports.del = function (req, res) {
-    var id = req.query.id;
-    if (id) {
-        Movie.remove({_id: id}, function (err, movie) {
-            if (err) {
-                console.log(err);
-                res.json({success: 0})
-            }
-            else {
-                res.json({success: 1})
-            }
-        })
-    }
+  var id = req.query.id;
+  if (id) {
+    Movie.remove({_id: id}, function (err, movie) {
+      if (err) {
+        console.log(err);
+        res.json({success: 0})
+      }
+      else {
+        res.json({success: 1})
+      }
+    })
+  }
 };
 
 exports.list = function (req, res) {
-    Movie.fetch(function (err, movies) {
-        if (err) console.log(err);
-        res.render('list', {
-            title: '列表页',
-            movies: movies
-        });
+  Movie.fetch(function (err, movies) {
+    if (err) console.log(err);
+    res.render('list', {
+      title: '列表页',
+      movies: movies
     });
+  });
 };
--- config/routes.js
+++ config/routes.js
@@ -1,29 +1,33 @@
 var Index = require('../app/controllers/index');
 var User = require('../app/controllers/user');
 var Movie = require('../app/controllers/movie');
+var Comment = require('../app/controllers/comment');
+
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        var _user = req.session.user;
-        app.locals.user = _user;
-        next();
-    });
+  app.use(function (req, res, next) {
+    var _user = req.session.user;
+    app.locals.user = _user;
+    next();
+  });
 
-    app.get('/', Index.index);
+  app.get('/', Index.index);
 
-    app.get('/movie/:id', Movie.detail);
-    app.get('/admin/movie', User.signinRequired, User.adminRequired, Movie.admin);
-    app.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired, Movie.update);
-    app.post('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.save);
-    app.delete('/admin/list', User.signinRequired, User.adminRequired, Movie.del);
-    app.get('/admin/list', User.signinRequired, User.adminRequired, Movie.list);
+  app.get('/movie/:id', Movie.detail);
+  app.get('/admin/movie', User.signinRequired, User.adminRequired, Movie.admin);
+  app.get('/admin/movie/update/:id', User.signinRequired, User.adminRequired, Movie.update);
+  app.post('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.save);
+  app.delete('/admin/list', User.signinRequired, User.adminRequired, Movie.del);
+  app.get('/admin/list', User.signinRequired, User.adminRequired, Movie.list);
 
-    app.post('/user/signup', User.signup);
-    app.get('/logout', User.logout);
-    app.post('/user/signin', User.signin);
-    app.get('/admin/userlist', User.signinRequired, User.adminRequired, User.list);
-    app.get('/signin', User.showSignIn);
-    app.get('/signup', User.showSignUp);
+  app.post('/user/signup', User.signup);
+  app.get('/logout', User.logout);
+  app.post('/user/signin', User.signin);
+  app.get('/admin/userlist', User.signinRequired, User.adminRequired, User.list);
+  app.get('/signin', User.showSignIn);
+  app.get('/signup', User.showSignUp);
+
+  app.post('/user/comment', User.signinRequired, Comment.save);
 };
 
 
